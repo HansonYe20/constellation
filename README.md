2b8eafd6f0cb2c98d83788306988d98fee04800e
叶俊 <yejun@xiejundeMacBook-Pro.local>
2020-05-23 18:35:08 +0800

    初始化项目以及mock服务搭建

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # constellation
 front end solutions
+前端工程化相关解决方案, 使用lerna来管理多库
+
+## Capriconr(摩羯座)
+  使用node+express+xxx搭建本地mock服务, 实现前后端分离开发
+
+## Aquarius(水瓶座)
+
+## Pisces(双鱼座)
+
+## Taurus(金牛座)
+
+## Gemini(双子座)
+
+## Cancer(巨蟹座)
+
+## Leo(狮子座)
+
+## Virgo(处女座)
+
+## Libra(天秤座)
+
+## Scorpio(天蝎座)
+
+## Sagittarius(射手座)
+
+
---
+++ packages/Capricorn/routes/files.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const findMock = require('../middlewares/findMockData');
+const _ = require('lodash');
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
+const { createCanvas } = require('canvas')
+
+// 有三种方法去处理文件/图片
+// 1. 直接使用res.sendFile(文件路径)
+// 2. 直接读取整个文件,使用res.end返回二进制数据
+// 3. 使用canvas来生成二进制数据返回图片, 自由度比较高
+router.get('/*', (req, res, next) => {
+
+  const filePath = path.join(__dirname, '../public/files/test.xlsx');
+  res.sendFile( filePath );
+
+});
+
+module.exports = router;
